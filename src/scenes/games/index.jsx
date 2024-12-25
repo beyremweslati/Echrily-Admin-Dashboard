148de851d1bf192311c4112043a2fe11a2568e09
Beyrem Oueslati <beyrem.weslati@gmail.com>
2024-12-25 13:06:40 +0100

    added toolbar to the datagrid to allow filtering and searching

--- src/scenes/games/index.jsx
+++ src/scenes/games/index.jsx
@@ -1,30 +1,26 @@
 import { Box, Typography, useTheme } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
-import { mockDataTeam } from "../../data/mockData";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import SportsEsportsOutlinedIcon from "@mui/icons-material/SportsEsportsOutlined";
-import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
-import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
 import Header from "../../components/Header";
 
 const Games = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
-    const [games,setGames] = useState([]);
+    const [games, setGames] = useState([]);
 
     useEffect(() => {
         const fetchGames = async () => {
-            try{
+            try {
                 const response = await axios.get("https://echrily.shop/api/games");
                 setGames(response.data);
-            }catch(err){
+            } catch (err) {
                 console.error("Failed to fetch games");
             }
-        }
+        };
 
-        fetchGames()
+        fetchGames();
     }, []);
 
     const columns = [
@@ -51,7 +47,7 @@ const Games = () => {
             field: "banner",
             headerName: "Banner",
             flex: 1,
-            renderCell: ({row: {banner} }) =>{
+            renderCell: ({ row: { banner } }) => {
                 return (
                     <Box
                         display="flex"
@@ -60,19 +56,19 @@ const Games = () => {
                         alignItems="center"
                         overflow="hidden"
                     >
-                        <img src={`https://echrily.shop/${banner}`} alt="banner" 
-                            
+                        <img
+                            src={`https://echrily.shop/${banner}`}
+                            alt="banner"
                             style={{
-                                width:"100%",
-                                height:"100%",
-                                objectFit:"contain",
-                                objectPosition:"center"
+                                width: "100%",
+                                height: "100%",
+                                objectFit: "contain",
+                                objectPosition: "center",
                             }}
-
                         />
                     </Box>
-                )
-            }
+                );
+            },
         },
         {
             field: "shop",
@@ -86,12 +82,12 @@ const Games = () => {
                         alignItems="center"
                     >
                         <img
-                            src={`../../../assets/${shop.split(' ').join("")}.svg`}
+                            src={`../../../assets/${shop.split(" ").join("")}.svg`}
                             alt="Platform"
                             height="34px"
                             width="34px"
                             style={{
-                                filter: theme.palette.mode === "dark" ? 'invert(1)' : 'none'
+                                filter: theme.palette.mode === "dark" ? "invert(1)" : "none",
                             }}
                         />
                     </Box>
@@ -103,36 +99,46 @@ const Games = () => {
     return (
         <Box m="20px">
             <Header title="GAMES" subtitle="Managing Games" />
-            <Box 
+            <Box
                 m="40px 0 0 0 "
                 height="75vh"
                 sx={{
-                    "& .MuiDataGrid-root" : {
-                        border :"none",
+                    "& .MuiDataGrid-root": {
+                        border: "none",
                     },
-                    "& .MuiDataGrid-cell" : {
-                        borderBottom :"none",
+                    "& .MuiDataGrid-cell": {
+                        borderBottom: "none",
                     },
-                    "& .name-column--cell" : {
+                    "& .name-column--cell": {
                         color: colors.blueAccent[500],
-                        fontSize:"14px",
-                        fontWeight: "bold"
+                        fontSize: "14px",
+                        fontWeight: "bold",
                     },
-                    "& .MuiDataGrid-columnHeader" : {
+                    "& .MuiDataGrid-columnHeader": {
                         backgroundColor: colors.primary[400],
-                        borderBottom:"none"
+                        borderBottom: "none",
                     },
-                    "& .MuiDataGrid-virtualScroller" : {
-                        backgroundColor:theme.palette.mode === "dark" ? colors.primary[700] : colors.grey[900]
+                    "& .MuiDataGrid-virtualScroller": {
+                        backgroundColor:
+                            theme.palette.mode === "dark"
+                                ? colors.primary[700]
+                                : colors.grey[900],
                     },
-                    "& .MuiDataGrid-footerContainer" : {
-                        borderTop:"none",
-                        backgroundColor: colors.primary[400]
+                    "& .MuiDataGrid-footerContainer": {
+                        borderTop: "none",
+                        backgroundColor: colors.primary[400],
                     },
-                    
+                    "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                        color: `${colors.grey[100]} !important`,
+                    },
                 }}
             >
-                <DataGrid rows={games} columns={columns} rowHeight={130}/>
+                <DataGrid
+                    rows={games}
+                    columns={columns}
+                    rowHeight={130}
+                    slots={{ toolbar: GridToolbar }}
+                />
             </Box>
         </Box>
     );
