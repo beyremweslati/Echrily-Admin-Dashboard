3a1e43ad39192efb09c631a67a586c3d633e90b5
Beyrem Oueslati <beyrem.weslati@gmail.com>
2025-01-26 14:34:42 +0100

    added token expiration check in ProtectedRoute component

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,4 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 const ProtectedRoute = () => {
   const token = localStorage.getItem("token");
@@ -6,7 +7,20 @@ const ProtectedRoute = () => {
   if (!token) {
     return <Navigate to="/login" />;
   }
-
+  try {
+    const decodedToken = jwtDecode(token);
+    const currentTime = Date.now() / 1000;
+    if (decodedToken.exp < currentTime) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      return <Navigate to="/login" />;
+    }
+  } catch (error) {
+    console.error("Token decoding error: ", error);
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    return <Navigate to="/login" />;
+  }
   return <Outlet />;
 };
 
